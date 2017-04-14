09c908e22a5eae41a81e92bbb829af032c69b43f
Josh <josh.karpel@gmail.com>
2017-04-13 23:45:13 -0500

    Implemented B-spline curves.

--- final_project.py
+++ final_project.py
@@ -43,7 +43,7 @@ class BSplineBasis:
     @memoize
     def basis_null(self, basis_function_index):
         """Return the zero-order basis function evaluated along the parameter space for a given basis function index."""
-        return np.where(np.greater_equal(self.xi, self.knot_vector[basis_function_index]) * np.less(self.xi, self.knot_vector[basis_function_index + 1]),
+        return np.where(np.greater_equal(self.xi, self.knot_vector[basis_function_index]) * np.less_equal(self.xi, self.knot_vector[basis_function_index + 1]),
                         1.0, 0.0)
 
     @memoize
@@ -67,6 +67,9 @@ class BSplineBasis:
 
             return first_term + second_term
 
+    def __iter__(self):
+        yield from (self.basis_function(basis_index, self.polynomial_order) for basis_index in self.basis_function_indices)
+
     def plot_basis_functions(self, **kwargs):
         fig = get_figure('full')
         ax = fig.add_subplot(111)
@@ -88,12 +91,55 @@ class BSplineBasis:
 
         ax.grid(True, **GRID_KWARGS)
 
-        save_current_figure(name = self.name + '__basis', target_dir = OUT_DIR, **kwargs)
+        save_current_figure(name = self.name + '__basis', **kwargs)
+
+        plt.close()
+
+
+class BSplineCurve:
+    def __init__(self, basis, control_points):
+        self.basis = basis
+        self.control_points = [np.array(control_point) for control_point in control_points]
+
+    def curve(self):
+        return sum(np.outer(basis_function, control_point) for basis_function, control_point in zip(self.basis, self.control_points)).T
+
+    def plot_curve(self, **kwargs):
+        """Only works in 2D..."""
+        fig = get_figure('full')
+        ax = fig.add_subplot(111)
+
+        curve_x, curve_y = self.curve()
+
+        control_points_x = np.array([control_point[0] for control_point in self.control_points])
+        control_points_y = np.array([control_point[1] for control_point in self.control_points])
+
+        x_min = min(np.min(curve_x), np.min(control_points_x))
+        x_max = max(np.max(curve_x), np.max(control_points_x))
+        x_range = np.abs(x_max - x_min)
+
+        y_min = min(np.min(curve_y), np.min(control_points_y))
+        y_max = max(np.max(curve_y), np.max(control_points_y))
+        y_range = np.abs(y_max - y_min)
+
+        ax.set_xlim(x_min - .1 * x_range, x_max + .1 * x_range)
+        ax.set_ylim(y_min - .1 * y_range, y_max + .1 * y_range)
+
+        ax.plot(control_points_x, control_points_y, color = 'C3', linestyle = '--', marker = 'o', linewidth = 1)
+        ax.plot(curve_x, curve_y, color = 'C0', linewidth = 2)
+
+        ax.axis('off')
+
+        save_current_figure(name = 'curve', **kwargs)
 
         plt.close()
 
 
 if __name__ == '__main__':
+    plt_kwargs = dict(
+        target_dir = OUT_DIR,
+    )
+
     bsplines = (
         BSplineBasis(knot_vector = [0, 1, 2, 3, 4, 5], polynomial_order = 0),
         BSplineBasis(knot_vector = [0, 1, 2, 3, 4, 5], polynomial_order = 1),
@@ -102,7 +148,6 @@ if __name__ == '__main__':
         BSplineBasis(knot_vector = [0, 0, 1, 2, 3, 4, 5], polynomial_order = 2),
         BSplineBasis(knot_vector = [0, 0, 0, 1, 2, 3, 4, 4, 5, 5, 5], polynomial_order = 2),
     )
-
     for bspline in bsplines:
         print(bspline.name)
 
@@ -113,6 +158,19 @@ if __name__ == '__main__':
         print('number of knots:', len(bspline.knot_vector))
         print('# of basis functions + polynomial order + 1:', len(bspline.basis_function_indices) + bspline.polynomial_order + 1)
 
-        bspline.plot_basis_functions()
+        bspline.plot_basis_functions(**plt_kwargs)
 
         print('-' * 80)
+
+    curve = BSplineCurve(BSplineBasis(knot_vector = [0, 0, 0, 1, 2, 3, 4, 4, 5, 5, 5], polynomial_order = 2), [
+        (0, 0),
+        (0, 1),
+        (1, -1),
+        (1, .5),
+        (2, .5),
+        (2.5, -1),
+        (3, 0),
+        (3.5, 0),
+    ])
+
+    curve.plot_curve(**plt_kwargs)
