b5deb3695648fbdc140210ec213636a2c08eeca1
JoshKarpel <josh.karpel@gmail.com>
2018-07-05 22:25:31 -0500

    add 3d curve

--- couch_club.py
+++ couch_club.py
@@ -180,3 +180,21 @@ if __name__ == '__main__':
         weights = [1, 2, 1, 3, .5, 1, 1, 1],
     )
     nurbs_curve.plot_curve_2D(name = 'nurbs_curve', **PLT_KWARGS)
+
+    fancy_curve = nurbs.BSplineCurve(
+        nurbs.BSplineBasis(
+            knot_vector = [0, 0, 0, 1, 2, 3, 4, 4, 5, 5, 5],
+            polynomial_order = 2,
+        ),
+        [
+            (0, 0, 0),
+            (0, 1, 1),
+            (1, -1, 2),
+            (1, .5, -.5),
+            (2, .5, 0),
+            (2.5, -1, 2),
+            (3, 0, -2),
+            (3.5, 0, 3),
+        ],
+    )
+    fancy_curve.plot_curve_3D(name = 'fancy_curve', **PLT_KWARGS)
